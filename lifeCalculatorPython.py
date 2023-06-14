cc86f586656d4d6bc409289e61a02bacbdf819f2
Zach Ruiz <powersbooth10@gmail.com>
2023-06-14 13:10:16 -0500

    refactored solution code.

--- lifeCalculatorPython.py
+++ lifeCalculatorPython.py
@@ -8,22 +8,32 @@ years_lived = int(age)
 
 years_max = 90
 
-days_max = years_max * 365
+years_left = years_max - years_lived
 
-weeks_max = years_max * 52
+# days_max = years_max * 365
 
-months_max = years_max * 12
+# weeks_max = years_max * 52
 
-days_lived = years_lived * 365
+# months_max = years_max * 12
 
-weeks_lived = years_lived * 52
+# days_lived = years_lived * 365
 
-months_lived = years_lived * 12
+# weeks_lived = years_lived * 52
 
-days_left = days_max - days_lived
+# months_lived = years_lived * 12
 
-weeks_left = weeks_max - weeks_lived
+# days_left = days_max - days_lived
 
-months_left = months_max - months_lived
+# weeks_left = weeks_max - weeks_lived
 
-print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left.")
+# months_left = months_max - months_lived
+
+days_left = years_left * 365
+
+weeks_left = years_left * 52
+
+months_left = years_left * 12
+
+message = f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left."
+
+print(message)
